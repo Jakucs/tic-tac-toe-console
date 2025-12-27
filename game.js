1d1c3a7d176f7c567a96c5dcc6392f1eed35f114
Jakucs Viktor <jakucs42@gmail.com>
2025-12-27 13:01:55 +0100

    displayBoard static works

--- game.js
+++ game.js
@@ -5,3 +5,25 @@ const prompt = promptSync({ sigint: true });
 /*function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }*/
+
+function getEmptyBoard(){
+    let board = [
+            [".", ".", "."],
+            ["." ,".", "."],
+            [".", ".", "."]
+    ]
+    return board
+}
+
+
+function displayBoard(board){
+    console.log(" ", "1", " ", "2", " ", "3")
+    console.log("A", board[0][0], "|", board[0][1], "|",  board[0][2])
+    console.log("----+---+----")
+    console.log("B", board[1][0], "|", board[1][1], "|", board[1][2])
+    console.log("----+---+----")
+    console.log("C", board[2][0], "|", board[2][1], "|", board[2][2])
+    
+}
+
+displayBoard(getEmptyBoard())
